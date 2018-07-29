4e47d9369d5839d4269921f6965cceb87179762f
natemoo-re <ntmoore@knox.edu>
2018-07-29 16:50:41 -0500

    Update serverless config?

--- .build/handler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var handlers_1 = require("./src/handlers");
-exports.hello = handlers_1.hello;
-exports.crc = handlers_1.crc;
-exports.listen = handlers_1.listen;
-//# sourceMappingURL=handler.js.map
